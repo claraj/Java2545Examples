de8a7facd5c8f99d10b6997fc050fabfaa07ce5e
clara <clara@clara.com>
2016-12-11 23:22:42 -0600

    JOptionPane - simple dialog examples

---
+++ src/week9_extra_gui_examples/custom_dialog/Main.java
@@ -0,0 +1,10 @@
+package week9_extra_gui_examples.custom_dialog;
+
+/**
+ * Created by admin on 12/11/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ShowDialogForm gui = new ShowDialogForm();
+    }
+}
