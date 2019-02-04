12586325ade8379f31653270e7e3e2cb442e5d00
clara <clara@minneapolis.edu>
2019-02-04 08:49:40 -0600

    exception handling vs. checks

--- src/main/java/week5_files_exceptions/TimingExceptions.java
+++ src/main/java/week5_files_exceptions/TimingExceptions.java
@@ -8,11 +8,13 @@ public class TimingExceptions {
     public static void main(String[] args) {
         
         
-        // A basic way of measuring time taken for excecution.
+        // A basic way of measuring time taken for execution.
         // Code profiling is a more sophisticated way to time parts of your code,
         // but this gives us some rough figures
         
         int repeats = 10000000;
+    
+        System.out.println("Repeat each task " + repeats + " times.");
         
         long start = System.currentTimeMillis();
         int totalChars = 0;
@@ -23,7 +25,7 @@ public class TimingExceptions {
         
         long time = end-start;
         System.out.println("Total chars:" + totalChars);
-        System.out.println("Time taken, with exception handling, " + time);  // 973 ms, about 1 second on my computer
+        System.out.println("Time taken for a null String, with exception handling: " + time);  // 521 ms, about half a second on my computer
     
     
         start = System.currentTimeMillis();
@@ -35,9 +37,34 @@ public class TimingExceptions {
     
         time = end-start;
         System.out.println("Total chars:" + totalChars);
-        System.out.println("Time taken, with a null check, " + time);  // 9 ms, about a one-hundredths of a second on my computer
+        System.out.println("Time taken for a null String, with a null check, " + time);  // 27 ms, twenty times faster than exception handling
     
     
+        // What about calling the code with non-null Strings?
+    
+        start = System.currentTimeMillis();
+        totalChars = 0;
+        
+        for (int x = 0 ; x < repeats ; x++) {
+            totalChars += countCharactersException("pizza");
+        }
+        end = System.currentTimeMillis();
+    
+        time = end - start;
+        System.out.println("Total chars:" + totalChars);
+        System.out.println("Time taken for a non-null String, with exception handling, " + time);  // 29 ms, about 1 second on my computer
+    
+    
+        start = System.currentTimeMillis();
+        totalChars = 0;
+        for (int x = 0 ; x < repeats ; x++) {
+            totalChars += countCharactersNullCheck("pizza");
+        }
+        end = System.currentTimeMillis();
+    
+        time = end - start;
+        System.out.println("Total chars:" + totalChars);
+        System.out.println("Time taken for a non-null String, with a null check, " + time);  // 40 ms - about 30% slower than the exception handling approach
     }
     
     
