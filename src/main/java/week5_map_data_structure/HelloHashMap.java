8b74bf8c4f594b57d2e5a0a983bda9d95b272acf
clara <clara@minneapolis.edu>
2019-09-11 08:34:25 -0500

    map examples

--- src/main/java/week5_map_data_structure/HelloHashMap.java
+++ src/main/java/week5_map_data_structure/HelloHashMap.java
@@ -23,24 +23,25 @@ public class HelloHashMap {
         h.put("1425", "Data Communications");
     
         // printing a HashMap. Note the order of keys is not the same as the order the key-value pairs were added
-        System.out.println(h);  // {1425=Data Communications, 1100=Info Tech Concepts, 1110=Info Tech Skills, 1250=Windows, 1150=Programming Logic}
+        System.out.println(h);
+        // {1425=Data Communications, 1100=Info Tech Concepts, 1110=Info Tech Skills, 1250=Windows, 1150=Programming Logic}
         
-        //Test some HashMap methods
-        
-        System.out.println("Does HashMap contain the key 1100? " + h.containsKey("1100"));
+        // Using some HashMap methods
         
         System.out.println("The value for the key 1150 is " + h.get("1150"));
         System.out.println("The value for the key 1999 is " + h.get("1999"));  // null, not error
+    
+        System.out.println("Does HashMap contain the key 1100? " + h.containsKey("1100"));   // true
+    
+        System.out.println("Is this HashMap empty? " + h.isEmpty());   // false
         
-        System.out.println("Is this HashMap empty? " + h.isEmpty());
+        System.out.println("How many key value pairs? " + h.size());   // 5
         
-        System.out.println("How many key value pairs? " + h.size());
+        System.out.println("Is there an entry with the value \"Windows\"? " + h.containsValue("Windows"));  // true
         
-        System.out.println("Is there an entry with the value \"Windows\"? " + h.containsValue("Windows"));
+        System.out.println("Delete the 1250 key-value pair by key. The value is " + h.remove("1250"));  // "Data Communications"
         
-        System.out.println("Delete the 1250 key-value pair, return the value. Value= " + h.remove("1250"));
-        
-        System.out.println("Delete a key that doesn't exist, what does this return? " + h.remove("1724"));
+        System.out.println("Delete a key that doesn't exist, what does this return? " + h.remove("1724"));   // null
         
         //Want to iterate over the keys? First get a set of all of the keys, then use a for each loop
         
@@ -48,22 +49,19 @@ public class HelloHashMap {
         
         System.out.println("\nGet a set of the keys, and loop over that set:\n");
         
-        // How do you print out all of the data - keys and values - in a HashMap?
-        
         for (String classNumber : h.keySet()) {
             //Use the key to get each value. Repeat for each key.
             System.out.println("Class Number = " + classNumber + " Class Name = " + h.get(classNumber) );
         }
-        
-        // Perhaps we just want to display the values.
         
         System.out.println("\nPrinting out all values using a loop\n");
         
         for (String value : h.values()) {
             System.out.println(value);
         }
+    
+        System.out.println("\nA loop for both keys and values\n");
         
-        // Or a loop for both keys and values
         for (Map.Entry<String, String> entry: h.entrySet()) {
             System.out.println(entry.getKey());
             System.out.println(entry.getValue());
