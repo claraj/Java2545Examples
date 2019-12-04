212e6842d9db8e1199a07225883ed460f44e3124
clara <clara@minneapolis.edu>
2019-12-04 12:01:14 -0600

    resizable table start

---
+++ src/main/java/week10_extra_gui_examples/hello_resizable_jtable/hello_jtable_resizable_abstract/Main.java
@@ -0,0 +1,10 @@
+package week10_extra_gui_examples.hello_resizable_jtable.hello_jtable_resizable_abstract;
+
+/**
+ * Created by clara on 4/18/18.
+ */
+public class Main {
+    public static void main(String[] args) {
+        HelloTableGUI gui = new HelloTableGUI();
+    }
+}
